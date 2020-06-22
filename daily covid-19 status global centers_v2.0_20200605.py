a4b0cee75b3d81fe760134c77cd9f6be7472265d
ymorzart <49669838+ymorzart@users.noreply.github.com>
2020-06-22 17:54:53 +0900

    Update daily covid-19 status global centers_v2.0_20200605.py

--- daily covid-19 status global centers_v2.0_20200605.py
+++ daily covid-19 status global centers_v2.0_20200605.py
@@ -13,9 +13,6 @@ Created on Fri Jun  5 23:28:24 2020
 #데이터 출처 : 위키백과European Centre for Disease Prevention and Control
 
 #import os
-#os.environ["HTTP_PROXY"] = "http://70.10.15.10:8080"
-#os.environ["HTTPS_PROXY"] = "http://70.10.15.10:8080"
-#os.environ["PYTHONHTTPSVERIFY"] = "0"
 
 import requests
 from bs4 import BeautifulSoup
